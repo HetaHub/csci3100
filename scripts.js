30b15911f84ecccb6fcd9e0acd5d7f0cf267de0d
ming0328ming <34433730+ming0328ming@users.noreply.github.com>
2021-03-19 16:25:58 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ function activateBar(id) {
 	x[0] = document.getElementById("gameBar");
 	x[1] = document.getElementById("accountBar")
 	x[2] = document.getElementById("leaderboardBar");
+	x[3] = document.getElementById("emptyBar");
 	var y = Array(3); 
 	y[0] = document.getElementById("gameBarButton");
 	y[1] = document.getElementById("accountBarButton");
@@ -12,11 +13,13 @@ function activateBar(id) {
 			x[i].style.display = "none";
 			y[i].classList.remove("active");
 		}
+		x[4].style.display = "none";
 		x[id].style.display = "block";
 		y[id].classList.add("active");
 	} else {
 		x[id].style.display = "none";
 		y[id].classList.remove("active")
+		x[4].style.display = "block";
 	}
 }
 
