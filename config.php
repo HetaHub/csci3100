08e765f2efe01e9cf499140ae55dc4054caf67f3
bri713 <62538275+bri713@users.noreply.github.com>
2021-04-13 22:58:46 +0800

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $host = "csci3100server.database.windows.net"; 
 $user = "csci3100admin"; 
 $password = "Csci3100pw"; 
-$dbname = "Csci3100db"; 
+$dbname = "csci3100db"; 
 
 $con = mysqli_connect($host, $user, $password,$dbname);
 if (!$con) {
