9f3b7b66f56ac4115aaa845e228640fcb5806033
bri713 <brian713ng@gmail.com>
2021-04-14 01:10:18 +0800

    change

--- regisscrip.js
+++ regisscrip.js
@@ -1,5 +1,5 @@
 var login= new Vue({
-    el: '#login',
+    el: '#register',
     data: {
         username: '',
         password: '',
@@ -9,12 +9,26 @@ var login= new Vue({
         processForm: function(){
             if(this.password!=this.repassword){
                 alert("password confirmation failed");
-                window.location.href = "registration.html";
+                Console.log("hi");
+                window.location.href = "main.html";
             }
             else{
                 //store into databas
-                window.location.href = "login.html";
+                this.record(this.username, this.password);
+                window.location.href = "main.html";
             }
+        },
+        record: function(){
+            axios.get('ajaxfile.php', {
+                params: {
+                  function: "register",
+                  username: this.username,
+                  password: this.password
+                }
+             })
+             .catch(function (error) {
+                console.log(error);
+             });
         }
     }
 })
