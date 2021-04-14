b9b98a8e1338e262b5559e374bcf8509945d5f71
HetaHub <54518395+HetaHub@users.noreply.github.com>
2021-04-14 14:14:38 +0800

    Add files via upload

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 
-$host = "csci3100server.database.windows.net"; 
-$user = "csci3100guest"; 
-$password = "Csci3100pw"; 
+$host = "localhost"; 
+$user = "csci3100admin"; 
+$password = "Csci3100vmpw"; 
 #<<<<<<< HEAD
 //$dbname = "Csci3100db";
 //=======
-$dbname = "csci3100db";
+$dbname = "users";
 //>>>>>>> a0e4387e944ced7e0f52c69ba424252a1d2e189a
 
 $con = mysqli_connect($host, $user, $password,$dbname);
