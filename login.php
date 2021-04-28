553550f745cf3b0a7c086cae00324ac3e5d77ed2
PusicatKen <opsken@live.hk>
2021-04-28 15:44:11 +0800

    login.php for login in function
    it works with my own xampp site

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+$conn = new mysqli("localhost", "root", "", 'users');
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$username=$_POST['username'];
+$password=$_POST['password'];
+ 
+$sql="select * from user where UserName='$username' AND Password='$password'";
+
+$result=mysqli_query($link,$sql);
+if($result->num_rows!=0){
+	echo 'Login in sucess';
+}else{
+	echo 'Wrong username or password!';
+}
+mysqli_close($link);
+$url = htmlspecialchars($_SERVER['HTTP_REFERER']);
+echo "<br><a href='$url'>Press here to Go Back</a>"; 
+?>
