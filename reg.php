a4c23a47c3de4cb93d77f3d71e917a34b1614389
PusicatKen <opsken@live.hk>
2021-04-30 00:24:43 +0800

    Check if username existed

--- reg.php
+++ reg.php
@@ -15,7 +15,18 @@ if ($conn->connect_error) {
 
 $username=$_POST['username'];
 $password=$_POST['password'];
- 
+
+$sql_check="select * from user where UserName='$username'";
+
+$result=mysqli_query($conn,$sql_check);
+if($result->num_rows!=0){
+	$conn->close();
+	echo 'This username has been used.';
+	$url = htmlspecialchars($_SERVER['HTTP_REFERER']);
+	echo "<br><a href='$url'>Press here to Go Back and name a new one.</a>"; 
+	exit;
+}
+
 $sql="insert into users (UserName, Password) values('$username','$password')";
 
 if ($conn->query($sql) === TRUE) {
