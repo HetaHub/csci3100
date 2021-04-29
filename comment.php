b79fe9ae1b540bc488650b180975e3027db3ad50
PusicatKen <opsken@live.hk>
2021-04-29 18:43:14 +0800

    Add files via upload

---
+++ comment.php
@@ -0,0 +1,21 @@
+<?php
+$conn = new mysqli("localhost", "root", "", 'users');
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$comment=htmlspecialchars($_POST['comment']);
+ 
+$sql="insert into comment (comment) values('$comment')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+  $url = htmlspecialchars($_SERVER['HTTP_REFERER']);
+  echo "<br><a href='$url'>Press here to Go Back</a>"; 
+?>
