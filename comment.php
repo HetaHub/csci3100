257d6ff13a15f625bc39b815ef17651aaabbb9cb
PusicatKen <opsken@live.hk>
2021-04-30 22:14:07 +0800

    Add comment

--- comment.php
+++ comment.php
@@ -1,21 +1,26 @@
 <?php
+//Connect to database comments
 $conn = new mysqli("localhost", "root", "", 'comments');
 
+//Report if cannot connect to database
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+//Get the input comment
 $comment=htmlspecialchars($_POST['comment']);
- 
-$sql="insert into comments (comments) values('$comment')";
 
+//Insert the comment to the database and return whether insertion is success or not.
+$sql="insert into comments (comments) values('$comment')";
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
+//Close the database connection
 $conn->close();
-  $url = htmlspecialchars($_SERVER['HTTP_REFERER']);
-  echo "<br><a href='$url'>Press here to Go Back</a>"; 
+//Button for returning to the previous page
+$url = htmlspecialchars($_SERVER['HTTP_REFERER']);
+echo "<br><a href='$url'>Press here to Go Back</a>"; 
 ?>
